ad6a3cf8894eaffcba3539f8606fdff6866aaba8
Juan Gamboa <juansimon.gr@gmail.com>
2020-08-10 18:01:29 -0500

    Initial commit: user model only

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,42 @@
+class SessionsController < ApplicationController
+  
+  skip_before_action :authorized, only: [:new, :create, :welcome]
+  
+  def new
+
+  end
+
+  def create
+
+    @user = User.find_by(username: params[:login_user][:username])
+
+    if @user && @user.authenticate(params[:login_user][:password])
+
+      session[:user_id] = @user.id
+
+      redirect_to '/welcome'
+
+    else
+      
+      flash[:alert] = 'User not found!'
+
+      redirect_to '/login'
+
+    end
+  end
+
+  def login
+  end
+
+  def welcome
+  end
+
+  def page_requires_login
+
+  end
+
+  def destroy
+    session.destroy()
+    redirect_to '/welcome'
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+
+    VALID_USERNAME_FORMAT = /[a-zA-Z0-9_]/i
+
+    validates :username, presence: true, length: { maximum: 28 },
+                         format: { with: VALID_USERNAME_FORMAT },
+                         uniqueness: { case_sensitive: true }
+
+    has_secure_password
+    validates  :password, presence: true, length: { minimum: 6 }
+    # has_many :ideas
+
+
+
+end
