660eed00034b2e839e22e426972c672c88ef9255
Juan Gamboa <juansimon.gr@gmail.com>
2020-08-14 03:21:30 -0500

    Before pushing to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,24 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Set up mailer configurations
+  config.action_mailer.default_url_options = { host: 'appforideas.herokuapp.com', protocol: 'https'}
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'appideas07@gmail.com'}
+
+  # Specific for gmail  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'appideas07',
+    password:             'p@55w0rd!',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
